56cfe0a25bc15704e213263aea5bf7e77d779b75
eduardobarona <beduardogiovanny@yahoo.com>
2023-11-27 14:16:17 -0500

    JavaScript's functions finished

--- javaScript/app.js
+++ javaScript/app.js
@@ -1,13 +1,23 @@
 let blenderStatus = "OFF";
 
 const blender = document.getElementById("blender");
-const button = document.getElementById("blenderButton");
+const button = document.getElementById("blender_button");
+const blenderSound = document.getElementById("blender_sound");
+const blenderButtonSound = document.getElementById("blender_button_sound");
 
-
-button.addEventListener('click', function() {
+button.addEventListener('click', ()=>{
     if(blenderStatus==="OFF"){
         blenderStatus = "ON";
+        blenderButtonSound.play();
+        setTimeout(function() {
+            blenderSound.play();;
+        }, 100);
+        blender.classList.add("active");
     }else{
         blenderStatus = "OFF";
-    }
-});
+        blender.classList.remove("active");
+        blenderButtonSound.play();
+        blenderSound.pause();
+        blenderSound.currentTime = 0;
+    };
+});  
